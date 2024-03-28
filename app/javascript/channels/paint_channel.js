2c074d6e24bb3c4bab7a3c1799a519e4422e040b
rapaccinim <marco.rapaccini@citizensadvice.org.uk>
2024-03-28 01:02:55 +0000

    Started to define the canvasSetup method that will be triggered when subscription is ready

--- app/javascript/channels/paint_channel.js
+++ app/javascript/channels/paint_channel.js
@@ -3,6 +3,17 @@ import consumer from "./consumer"
 consumer.subscriptions.create("PaintChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    this.canvasSetup();
+  },
+
+  canvasSetup() {
+    // usual stuff related to canvas
+    this.canvas = document.getElementById("jamboard");
+    this.context = this.canvas.getContext("2d");
+    // we also need a remote context (what gets the receiver of the data)
+    this.remoteContext = this.canvas.getContext("2d");
+
+    // TBD...
   },
 
   disconnected() {
